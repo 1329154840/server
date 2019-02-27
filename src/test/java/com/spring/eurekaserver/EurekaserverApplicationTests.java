4a2d227346066ff1f051e0a5cad9c6046aa9564e
yezuoyao <1329154840@qq.com>
2019-02-27 09:55:18 +0800

    测试推送

--- src/test/java/com/spring/eurekaserver/EurekaserverApplicationTests.java
+++ src/test/java/com/spring/eurekaserver/EurekaserverApplicationTests.java
@@ -1,5 +1,6 @@
 package com.spring.eurekaserver;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,10 +8,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class EurekaserverApplicationTests {
 
     @Test
     public void contextLoads() {
+        log.info("hello");
     }
 
 }
